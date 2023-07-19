773c0c49e9dd5f99cab14ba289ea4194bcaffc3e
Ray Benvinda Trajano <121032249+Ray-BT@users.noreply.github.com>
2023-07-19 15:34:10 -0300

    começando a fazer o script

--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
-document.getElementById("idia")
-document.getElementById("imes")
-document.getElementById("iano")
+function calcularIdade() {
+let fdia = document.getElementById("idia")
+let fmes = document.getElementById("imes")
+let fano = document.getElementById("iano")
+let data = new Date
+let anoAtual = data.getFullYear()
+let dataDia = new Date
+let diaAtual = dataDia.getDay()
+let dataMes = new Date
+let mesAtual = dataMes.getMonth()
 
-
+    let ano = anoAtual - Number(fano.value) 
+    let mes = 
+    let dia =
+}
