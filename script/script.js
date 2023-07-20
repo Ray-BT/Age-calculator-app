c05905f3dd192d48e75809178e77da90fc04ce54
Ray Benvinda Trajano <121032249+Ray-BT@users.noreply.github.com>
2023-07-20 16:33:33 -0300

    terminando o script

--- script/script.js
+++ script/script.js
@@ -1,15 +1,24 @@
 function calcularIdade() {
-let fdia = document.getElementById("idia")
-let fmes = document.getElementById("imes")
-let fano = document.getElementById("iano")
-let data = new Date
-let anoAtual = data.getFullYear()
-let dataDia = new Date
-let diaAtual = dataDia.getDay()
-let dataMes = new Date
-let mesAtual = dataMes.getMonth()
-
-    let ano = anoAtual - Number(fano.value) 
-    let mes = 
-    let dia =
-}
+    let idia = document.getElementById("idia").value;
+    let imes = document.getElementById("imes").value;
+    let res = document.querySelector('div#res');
+    let iano = document.getElementById("iano").value;
+    
+    
+    let dataAtual = new Date();
+    let anoAtual = dataAtual.getFullYear();
+    let mesAtual = dataAtual.getMonth() + 1;
+    let diaAtual = dataAtual.getDate();
+  
+    let idadeAno = anoAtual - Number(iano);
+    let idadeMes = (idadeAno * 12) + (mesAtual - Number(imes));
+    
+    let dataNascimento = new Date(iano, imes - 1, idia);
+    let idadeDias = Math.floor((dataAtual - dataNascimento) / (1000 * 60 * 60 * 24));
+    
+    res.innerHTML += `${idadeAno}<br>`
+    res.innerHTML += `${idadeDias}<br>`
+    res.innerHTML += `${idadeMes}`
+    
+  }
+  
