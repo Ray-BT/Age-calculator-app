c197305fae0869d6b8f056d2a81b6c8ff25339cc
Ray Benvinda Trajano <121032249+Ray-BT@users.noreply.github.com>
2023-07-21 18:34:41 -0300

    Correção da logica do script

--- script/script.js
+++ script/script.js
@@ -1,24 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let res = document.querySelector('div#res');
+  res.innerHTML = `-- years<br>-- days<br>-- months`;
+});
+
 function calcularIdade() {
-    let idia = document.getElementById("idia").value;
-    let imes = document.getElementById("imes").value;
-    let res = document.querySelector('div#res');
-    let iano = document.getElementById("iano").value;
-    
-    
-    let dataAtual = new Date();
-    let anoAtual = dataAtual.getFullYear();
-    let mesAtual = dataAtual.getMonth() + 1;
-    let diaAtual = dataAtual.getDate();
+  let idia = document.getElementById("idia").value;
+  let imes = document.getElementById("imes").value;
+  let iano = document.getElementById("iano").value;
+  let res = document.querySelector('div#res');
   
-    let idadeAno = anoAtual - Number(iano);
-    let idadeMes = (idadeAno * 12) + (mesAtual - Number(imes));
-    
-    let dataNascimento = new Date(iano, imes - 1, idia);
-    let idadeDias = Math.floor((dataAtual - dataNascimento) / (1000 * 60 * 60 * 24));
-    
-    res.innerHTML += `${idadeAno}<br>`
-    res.innerHTML += `${idadeDias}<br>`
-    res.innerHTML += `${idadeMes}`
-    
-  }
+  let dataAtual = new Date();
+  let anoAtual = dataAtual.getFullYear();
+  let mesAtual = dataAtual.getMonth() + 1;
+  let diaAtual = dataAtual.getDate();
+
+  let idadeAno = anoAtual - Number(iano);
+  let idadeMes = (idadeAno * 12) + (mesAtual - Number(imes));
+  
+  let dataNascimento = new Date(iano, imes - 1, idia);
+  let idadeDias = Math.floor((dataAtual - dataNascimento) / (1000 * 60 * 60 * 24));
+
+  res.innerHTML = `${idadeAno} years<br>${idadeDias} days<br>${idadeMes} months`;
+}
   
